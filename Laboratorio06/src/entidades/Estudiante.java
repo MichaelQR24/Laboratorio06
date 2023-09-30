e42946341dceba9349301315190d03bccdf6eeb0
MichaelQR24 <141574326+MichaelQR24@users.noreply.github.com>
2023-09-29 22:38:53 -0500

    Add files via upload

---
+++ Laboratorio06/src/entidades/Estudiante.java
@@ -0,0 +1,54 @@
+
+package entidades;
+
+import java.util.List;
+
+public class Estudiante {
+    
+     private List<Curso> cursos;
+    private String codigo;
+    private String nombre;
+
+    /**
+     * @return the cursos
+     */
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    /**
+     * @param cursos the cursos to set
+     */
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+   
+}
---
+++ Laboratorio06/src/entidades/Profesor.java
@@ -0,0 +1,39 @@
+
+package entidades;
+
+import java.util.List;
+
+public class Profesor {
+    
+    private String nombre;
+    private List<Curso> cursos;
+    
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cursos
+     */
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    /**
+     * @param cursos the cursos to set
+     */
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+}
